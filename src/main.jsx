8dd5270b2baf98780c6f44b683cd32ca575d77c8
cjo3o <cjy55973743@gmail.com>
2025-04-01 18:11:13 +0900

    파일 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Index from './Index.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <Index />
 )
