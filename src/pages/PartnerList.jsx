bf315bb6e7503db5a3dd2ecf633747ce26aaed86
DESKTOP-29VI7GO\user <kimyoott@gmail.com>
2025-04-07 15:04:38 +0900

    no message

--- src/pages/PartnerList.jsx
+++ src/pages/PartnerList.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 function PartnerList() {
     return (
-        <div>제휴숙소목록</div>
+        <>
+            <div className='main'>
+                <div className='header'>제휴숙소관리</div>
+                <div className='card'>
+                    <h3>제휴숙소목록</h3>
+                </div>
+            </div>
+        </>
     );
 }
 
