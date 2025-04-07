cf8793516c495bfad5823a15b4908e001efa56aa
DESKTOP-29VI7GO\user <kimyoott@gmail.com>
2025-04-07 18:07:47 +0900

    0407 제휴숙소관리 만들기 css폴더 추가

---
+++ src/lib/supabase.js
@@ -0,0 +1,7 @@
+import {createClient} from '@supabase/supabase-js';
+
+// Supabase 프로젝트 URL과 키
+const supabaseUrl = 'https://zgrjjnifqoactpuqolao.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpncmpqbmlmcW9hY3RwdXFvbGFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyNDc0NTgsImV4cCI6MjA1NjgyMzQ1OH0._Vl-6CRKdMjeDRyNoxlfect7sgusZ7L0N5OYu0a5hT0';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
