0e0fc43851a23e49df301ba094b553e54a7e1a86
cjo3o <cjy55973743@gmail.com>
2025-04-15 18:13:06 +0900

    no message

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -10,7 +10,10 @@ function EmployeeList(props) {
     const [rowdata, setRowdata] = useState([]);
     const [status, setStatus] = useState('');
     const [loading, setLoading] = useState(false);
-    const [open, setOpen] = useState(false);
+    const [openMemo, setOpenMemo] = useState(false);
+    const [memoValue, setMemoValue] = useState('');
+    const [openEdit, setOpenEdit] = useState(false);
+    const [selectedEmployee, setSelectedEmployee] = useState(null);
     const [messageApi, contextHolder] = message.useMessage();
     const success = () => {
         messageApi.open({
@@ -19,26 +22,59 @@ function EmployeeList(props) {
         });
     };
 
-    const showModal = () => {
-        setOpen(true);
+    const showMemo = (employee) => {
+        setSelectedEmployee(employee);
+        setMemoValue(employee.memo || '');
+        setOpenMemo(true);
     };
 
-    const handleOk = () => {
-        success();
+    const showEdit = (employee) => {
+        setSelectedEmployee(employee)
+        setOpenEdit(true);
+    };
+
+    const handleOk = async () => {
         setLoading(true);
+
+        if (openMemo && selectedEmployee) {
+            await supabase.from('employees')
+                .update({memo: memoValue})
+                .eq('no', selectedEmployee.no);
+
+            const {data} = await supabase.from('employees').select().order('no', {ascending: true});
+            setRowdata(data);
+        }
+        if (openEdit && selectedEmployee) {
+            await supabase.from('employees')
+                .update({
+                    name: selectedEmployee.name,
+                    email: selectedEmployee.email,
+                    department: selectedEmployee.department,
+                    position: selectedEmployee.position,
+                    role: selectedEmployee.role,
+                    status: selectedEmployee.status,
+                })
+                .eq('no', selectedEmployee.no);
+
+            const {data} = await supabase.from('employees').select().order('no', {ascending: true});
+            setRowdata(data);
+        }
+        success();
         setTimeout(() => {
             setLoading(false);
-            setOpen(false);
+            setOpenMemo(false);
+            setOpenEdit(false);
         }, 2000);
     };
 
     const handleCancel = () => {
-        setOpen(false);
+        setOpenMemo(false);
+        setOpenEdit(false);
     };
 
     useEffect(() => {
         async function fetchEmployees() {
-            const res = await supabase.from('employees').select();
+            const res = await supabase.from('employees').select().order('no', {ascending: true});
             setRowdata(res.data);
             console.log(res.data);
         }
@@ -88,13 +124,14 @@ function EmployeeList(props) {
                                                 {value: '차단', label: <span>차단</span>}
                                             ]}/>
                                 </td>
-                                <td><Button color="default" variant="filled" onClick={showModal}>
+                                <td><Button color="default" variant="filled" onClick={() => showMemo(item)}>
                                     메모
                                 </Button></td>
                                 <td>
-                                    <div style={{ display: "flex", gap: "10px", justifyContent: "center" }}>
-                                        <Button icon={<EditOutlined />} shape="square" size="medium" />
-                                        <Button icon={<DeleteOutlined />} shape="square" size="medium" />
+                                    <div style={{display: "flex", gap: "10px", justifyContent: "center"}}>
+                                        <Button icon={<EditOutlined/>} shape="square" size="medium"
+                                                onClick={() => showEdit(item)}/>
+                                        <Button icon={<DeleteOutlined/>} shape="square" size="medium"/>
                                     </div>
                                 </td>
 
@@ -105,7 +142,7 @@ function EmployeeList(props) {
                 </div>
                 <Modal
                     title="메모"
-                    open={open}
+                    open={openMemo}
                     onOk={handleOk}
                     onCancel={handleCancel}
                     footer={[
@@ -116,7 +153,73 @@ function EmployeeList(props) {
                             저장
                         </Button>
                     ]}>
-                    <TextArea rows={4}/>
+                    <TextArea
+                        rows={4}
+                        value={memoValue}
+                        onChange={(e) => setMemoValue(e.target.value)}
+                    />
+                </Modal>
+                <Modal
+                    title="수정"
+                    open={openEdit}
+                    onOk={handleOk}
+                    onCancel={handleCancel}
+                    footer={[
+                        <Button key="back" onClick={handleCancel}>
+                            닫기
+                        </Button>,
+                        <Button key="submit" type="primary" loading={loading} onClick={handleOk}>
+                            저장
+                        </Button>
+                    ]}>
+                    <div className="editContent" style={{display: "flex", flexDirection: "column", gap: "20px"}}>
+                        <Input
+                            placeholder="이름"
+                            value={selectedEmployee?.name}
+                            onChange={(e) => setSelectedEmployee({...selectedEmployee, name: e.target.value})}
+                        />
+                        <Input
+                            placeholder="이메일"
+                            value={selectedEmployee?.email}
+                            onChange={(e) => setSelectedEmployee({...selectedEmployee, email: e.target.value})}
+                        />
+                        <Input
+                            placeholder="부서"
+                            value={selectedEmployee?.department}
+                            onChange={(e) => setSelectedEmployee({...selectedEmployee, department: e.target.value})}
+                        />
+                        <Input
+                            placeholder="직위"
+                            value={selectedEmployee?.position}
+                            onChange={(e) => setSelectedEmployee({...selectedEmployee, position: e.target.value})}
+                        />
+                        <div style={{display: "flex", gap: "20px"}}>
+                            <div style={{display: "flex", flexDirection: "column", gap: "0.5rem"}}>
+                                <span>권한</span>
+                                <Select value={selectedEmployee?.role}
+                                        style={{'width': '100px'}}
+                                        onChange={(value) => setSelectedEmployee({...selectedEmployee, role: value})}
+                                        options={[
+                                            {value: '읽기전용', label: <span>읽기전용</span>},
+                                            {value: '수정가능', label: <span>수정가능</span>},
+                                            {value: '관리자', label: <span>관리자</span>}
+                                        ]}
+                                />
+                            </div>
+                            <div style={{display: "flex", flexDirection: "column", gap: "0.5rem"}}>
+                                <span>상태</span>
+                                <Select value={selectedEmployee?.status}
+                                        style={{'width': '90px'}}
+                                        onChange={(value) => setSelectedEmployee({...selectedEmployee, status: value})}
+                                        options={[
+                                            {value: '인증중', label: <span>인증중</span>},
+                                            {value: '사용중', label: <span>사용중</span>},
+                                            {value: '차단', label: <span>차단</span>}
+                                        ]}
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </Modal>
             </div>
         </>
